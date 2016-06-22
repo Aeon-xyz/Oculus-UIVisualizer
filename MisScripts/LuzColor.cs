6f68c9de154a9340525ac492409b54b79259c39c
Aeon-xyz <enum.script@gmail.com>
2016-06-22 00:16:52 -0500

    First Commit
    First Commit

---
+++ MisScripts/LuzColor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class LuzColor : MonoBehaviour {
+    public Light miLuz;
+    public Slider miSlider;
+    public void LuzRoja()
+    {
+        miLuz.color = Color.red;
+    }
+    public void LuzVerde()
+    {
+        miLuz.color = Color.green;
+    }
+    public void LuzAmarilla()
+    {
+        miLuz.color = Color.yellow;
+    }
+    public void LuzAzul()
+    {
+        miLuz.color = Color.blue;
+    }
+
+    public void IntensidadLuz()
+    {
+        miLuz.intensity = miSlider.value * 8f;
+    }
+}
